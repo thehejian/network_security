2fd35fc5b264f0d299f3013e268cbf6f0a1152c2
thehejian <745544752@qq.com>
2021-12-07 13:06:24 +0800

    Create 005-云计算-产品解析.sh

---
+++ 005-云计算-产品解析.sh
@@ -0,0 +1,22 @@
+#云计算及产品解析
+        #认识云计算
+                #什么是云计算
+                        #基于互联网处理计算请求并交付资源的服务模式
+                        #客户端只关注资源的可用性（提需求、取结果）
+                        #服务端/云端组织运算处理
+                #云计算三大服务模式      https://www.zhihu.com/question/21641778
+                        #SAAS           软件、应用                  Software-as-a-Service
+                        #PAAS           平台、运行环境               Platform-as-a-Service
+                        #IAAS           基础设施                    Infrastructure-as-a-Service
+                #虚拟化与云计算          https://www.zhihu.com/question/22793847/answer/556691008
+                        #Virtualization 虚拟化
+                                #针对物理资源的逻辑化，是实现云计算的关键手段之一
+                                #从服务方定义，底层对用户是透明的                                
+        #云服务商及产品
+                #知名云服务商
+                        #国内：华为云、阿里云、百度云、腾讯云...
+                        #国外：亚马逊AWS、微软Azure、IBM SCE+...
+                #云计算产品选购建议
+                        #服务商的知名度、靠谱度
+                        #以实际需求为本；云主机、数据库、CDN、安全等，选择性价比最优的
+                #华为云产品介绍
