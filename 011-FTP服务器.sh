b8f876618c819e5f2521b63442ef6a7a2a27c8af
thehejian <745544752@qq.com>
2021-12-10 21:31:06 +0800

    Create 011-FTP服务器.sh

---
+++ 011-FTP服务器.sh
@@ -0,0 +1,12 @@
+#win FTP软件
+#FileZilla server
+#https://filezilla-project.org/download.php?type=server
+#https://osdn.net/projects/filezilla/downloads/67734/FileZilla_Server-0_9_60.exe/
+#anonymous（匿名）
+
+#安装FileZilla_Server-0_9_60
+#Edit——General——User——add（anonymous）
+#Edit——General——Shared folders
+
+#访问
+#ftp://127.0.0.1
