7916578dad728e4b341581ae6ed040e4c9222d96
thehejian <745544752@qq.com>
2021-12-12 13:50:29 +0800

    Create 012-web服务器-IIS.sh

---
+++ 012-web服务器-IIS.sh
@@ -0,0 +1,16 @@
+#web服务器概述
+#B/S架构
+#browser/Server架构
+  #服务端支持http协议
+  #浏览器/客户端：下载按标记规范显示的网页的浏览器程序
+  
+#如何获取网页资源
+#URL网址
+  #uniform Resource locator 统一资源定位器
+  #资源类别：//服务器地址/目标路径/文件名
+  
+IIS建站
+Internet Informatica Service
+  微软提供的web服务产品
+  可以通过图形管理web/FTP
+  
