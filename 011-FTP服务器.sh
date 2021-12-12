a4f5f98c2c70cc2c1641685543dfed76fdbc7df2
thehejian <745544752@qq.com>
2021-12-12 10:32:44 +0800

    Update 011-FTP服务器.sh

--- 011-FTP服务器.sh
+++ 011-FTP服务器.sh
@@ -1,4 +1,9 @@
 #win FTP软件
+#基于CS架构的文件上传下载通信
+    #服务器端：提供资源的上传下载
+    #客户端：与服务端通信，来实现下载、上传文档
+#软件 FileZilla Server    ServU   vsftpd
+
 #FileZilla server
 #https://filezilla-project.org/download.php?type=server
 #https://osdn.net/projects/filezilla/downloads/67734/FileZilla_Server-0_9_60.exe/
