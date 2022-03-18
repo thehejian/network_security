fcd35d32ecae77fe68e8f71d83cbb3777be472e0
hejianone <hejianone@gmail.com>
2022-03-18 15:09:37 +0800

    20220318 15:09:37

---
+++ 028-luyou.sh
@@ -0,0 +1,20 @@
+luyouqi
+fuze butong wangluo zhijian zhuanfa shuju
+	mubiao lujing fuza,luyouqi jueding zuijia lujing
+	yekeyi zhilian zhuji chongdang wangguan
+wangluo hulian zui hexin shebei
+gongzuo yuanli 
+weihu yifen luyoubiao
+	yaofangwen de wangluo ying cong nayige jiekou fasong
+	daoda xiayige luyouqi(zhuji) de dizhi
+ruhe gongzuo
+	jiancha shujubao de mubiao IP dizhi
+	luyoubiao chaxun daoda mubiao de xianlu,bing xuanze zuijia xianlu
+	anzhao zuijia xianlu zhuanfa shujubao
+zhuji de wangguan dizhi jiushi lianjie luyouqi de jiekou IP
+<Huawei>system-view
+[Huawei]sysname R1
+[R1]interface GigabitEthernet 0/0/0
+#[R1-GigabitEthernet0/0/0]ip address 192.168.1.254 24
+#sike buzhichi shangmian de caozuo
+[R1-GigabitEthernet0/0/0]ipaddress192.168.1.254 255.255.255.0
