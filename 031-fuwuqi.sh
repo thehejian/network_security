54cfbe0fb12e23dcce153a466af93a55d8ca2510
thehejian <745544752@qq.com>
2022-04-14 09:20:14 +0800

    20220414 09:20:14

---
+++ 031-fuwuqi.sh
@@ -0,0 +1,96 @@
+shenmeshi fuwuqi
+zai wangluo huanjing zhong,wei yonghu tigong yiding ziyuan huo fuwu de yingyong ruanjian,yiji zhicheng ci pingtai de gaoxingneng jisuanji
+
+IDC
+Internet ZData Center
+hulianwang shujuzhongxin
+
+fuweuqi de guige fenlei
+jijiashi fuwuqi
+	bianpingshi,biaozhun kuandu 19 yinbgcun
+	shihe piliang shangjia dao jigui
+tashi fuwuqi
+daopianfuwuqi
+jiguishi fuwuqi
+
+houdu ji jigui chicun
+houdu guige
+	1U=1Unit=1.75yingcun=4.445cm
+fuwuqi 1U\2U\4U
+jiguirongliang 18U\22U\37U\42U
+	wangluo jigui:shen600\800mm
+	fuwuqi jigui:shen900\1000mm
+
+CPU chuliqi
+zhichi chuliqi geshu
+	danlu\shuanglu\silu
+chuli hexinshu\pinlv\huancun
+	4he\8he\16he\18he
+	2.4GHz\3.2GHz\3.5GHz
+	15MB\24MB\45MB
+
+neicun
+neicun chacao geshu 
+	8\12\16\24\48
+fuwuqi zhichi zuida neicun rongliang
+	64GB\128GB\192GB
+	768GB\1.5TB
+
+cipan
+yingpan chicun ji jiekou
+	2.5cun SAS jiekou  \  2.5cun SSD gutai pan
+	3.5cun SATAjiekou
+panjia geshu 
+	4\8\12\16
+fuwuqi zuida zhichi de yingpan rongliang
+	4\8\12TB
+
+RAID zhenlie ka
+RAID
+	redundant arrray of inexpensive disks
+	lianjia rongyu cipan zhenlie
+zhichi zhenlie jishu leibie
+	RAID0   tiaodai
+		>=2	100%	wu jiaoyan	duxie sudu gao
+	RAID1   jingxiang
+		>=2	<=50%	wu jiaoyan	duxie sudu di	
+	RAID10  fuhe
+		>=4	<=50%	wu jiaoyan	duxie sudu zhong
+	RAID5   junheng 
+		>=3	n-1/n	jiaoyanpan 1	duxie sudu jiaogao
+	RAID6   junheng
+		>=4	n-2/n	jiaoyanpan 2	duxie sudu jiaogao
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
