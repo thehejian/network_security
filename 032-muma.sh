cdbf135d2193d60654ae2e360a95d31dac8a5e54
thehejian <745544752@qq.com>
2022-04-14 11:35:59 +0800

    20220414 11:35:59

--- 032-muma.sh
+++ 032-muma.sh
@@ -1,7 +1,15 @@
 muma yu bingdu
 jisuanji muma
-
+	muma gaishu
+		jisuanji muma Trojan
+		juyou qipian xingzhi,hui daozhi shouhaizhe jisuanji de mingan xinxi xielou,bei heike kongzhi deng huihai de yinbi xing chengxu
+		tongchang bei weizhuang yifu zai gongju ruanjian xiaoyouxi tupian shipin deng wenjian zhong, youshi yonghu dakai yi jihuo muma 
+	muma chengxu yanshi
 jisuanji bingdu
-
+	bingdu gailan
+	bingdu chengxu yanshi
 xitong anquan fangyu
+	fang bingdu\fang muma ruanjian
+	shiyong 360 chasha bingdu muma
+	anquan gongfang jishu qianzhan
 
