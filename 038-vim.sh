00f13077f4c3fa8d4affc91ac1184d43a594c287
thehejian <745544752@qq.com>
2022-04-25 15:01:33 +0800

    20220425 15:01:33

---
+++ 038-vim.sh
@@ -0,0 +1,21 @@
+gg	tiaozhuan dao diyi hang
+4G	tiaozhuan dao disi hang
+G	tiaozhuan dao zuihou yihang
+yy	fuzhi dangqian hang
+4yy	fuzhi disi hang
+p	zhantie
+dd	shanchu dangqian hang
+4dd	shangchu disi hang
+u	chexiao yici caozuo
+U	chexiao dangqian hang suoyou caozuo
+Ctrl+r	chongzuo zuijin yici bei chexiao de caozuo
+/123	chazhao 123
+n	xia yige
+N	shang yige
+:s/old/new/g		hangnei tihuan
+:2,9 s/old/new/g	2 dao 9 hang tihuan
+:% s/old/new/g		quanwen tihuan
+:set nu			xianshi hanghao
+:set nonu		bu xianshi hanghao	
+
+
