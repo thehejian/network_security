7ea62dc16c9bef3491e58fa96e3285e24ffb856f
thehejian <745544752@qq.com>
2022-04-26 17:18:50 +0800

    20220426 17:18:50

--- 043-jisuanji_xuanxing.sh
+++ 043-jisuanji_xuanxing.sh
@@ -1,1 +1,16 @@
 jisuan ji zucheng
+
+xianshiqi jiekou
+	HDMI gao qingxi du duo meiti(yinpin + shipin) jiekou
+	laoshi
+		DVI shipin shuzi jiekou
+		VGA shipin tuzhen jiekou
+
+cunchu qi
+neicun
+	RAM suiji fangwen cunchu qi: kedu kexie yidiushi;
+	ROM zhidu cunchuqi:kedu bukexie buyi diushi
+waicun
+
+
+
